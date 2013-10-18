01d86ee6a4f5924b9cb541594d009a08bcb72839
leiming <leiminghhu@gmail.com>
2013-10-18 18:10:48 +0800

    add magazine.html

--- js/controller.js
+++ js/controller.js
@@ -2,20 +2,35 @@
 
 var neikanControllers = angular.module('neikanControllers', []);
 
-neikanControllers.controller('GalleryCtrl', ['$scope',
-  function GalleryCtrl($scope) {
-    $scope.magazines = [
-      {id: "cover01", name: '第一期', link: "01", thumbnail: ""},
-      {id: "cover02", name: '第二期', link: "02", thumbnail: ""},
-      {id: "cover03", name: '第三期', link: "03", thumbnail: ""},
-      {id: "cover04", name: '第四期', link: "04", thumbnail: ""},
-      {id: "cover05", name: '第五期', link: "05", thumbnail: ""},
-      {id: "cover06", name: '第六期', link: "06", thumbnail: ""}
-    ];
+neikanControllers.controller('GalleryCtrl', ['$scope', GalleryCtrl]);
+neikanControllers.controller('MagazineCtrl', ['$scope', '$routeParams', "$http" , MagazineCtrl]);
 
-    $scope.clickImg = function () {
-      console.log("asdf");
-    };
+function GalleryCtrl($scope) {
+  $scope.magazines = [
+    {id: "01", name: '第一期', link: "01", thumbnail: ""},
+    {id: "02", name: '第二期', link: "02", thumbnail: ""},
+    {id: "03", name: '第三期', link: "03", thumbnail: ""},
+    {id: "04", name: '第四期', link: "04", thumbnail: ""},
+    {id: "05", name: '第五期', link: "05", thumbnail: ""},
+    {id: "06", name: '第六期', link: "06", thumbnail: ""}
+  ];
 
-  }]);
+};
+
+function MagazineCtrl($scope, $routeParams, $http) {
+  $scope.magId = $routeParams.magId;
+/*  $http.get('/resource/details.json').success(function (data) {
+    $scope.details = data;
+
+  });*/
+
+  $scope.magLength = 7;
+
+  $scope.magImages = [];
+
+  for (var i = 0; i < $scope.magLength -1; i++) {
+    $scope.magImages.push(i+1);
+  }
+
+}
 
--- js/neikan.js
+++ js/neikan.js
@@ -1,15 +1,21 @@
-"use strict";
+'use strict';
 
-var neikanApp = angular.module('neikanApp', ['ngRoute', 'neikanControllers']);
+/* App Module */
 
-neikanApp.config(['$routeProvider', function ($routeProvider, $locationProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'gallery.html',
-    controller : 'GalleryCtrl'}).otherwise({
-        redirectTo: '/'
+
+var myApp = angular.module('neikanApp', [
+      'ngRoute',
+      'neikanControllers'])
+    .config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.
+      when('/', {
+        templateUrl: 'gallery.html',
+        controller : 'GalleryCtrl'}).
+      when('/magazine/:magId', {
+        templateUrl: 'magazine.html',
+        controller : 'MagazineCtrl'
       });
 
   // configure html5 to get links working on jsfiddle
   // $locationProvider.html5Mode(true);
 }]);
-
